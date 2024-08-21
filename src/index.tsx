fa96930db39a4fbbbfa362f7a2602887db5a1af9
AudreyGentili <audrey.gentili@zenika.com>
2024-08-21 10:44:23 +0200

    --amend

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom/client";
-import './index.css';
-import App from './App.tsx';
+import { HashRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
-    <App/>
+    <HashRouter>
+      {" "}
+      <App />{" "}
+    </HashRouter>{" "}
   </React.StrictMode>
 );
